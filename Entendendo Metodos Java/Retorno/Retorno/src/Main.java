8a0e2fedfc70c80f563e2c2c2a3866119ec4dcc2
JSilva-Erick <erickjorgesilva@gmail.com>
2022-09-20 15:32:55 -0300

    Métodos Java

---
+++ Entendendo Metodos Java/Retorno/Retorno/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        //Retornos
+        System.out.println("Exercícios retornos");
+
+        double areaQuadrado = Quadrilatero.area(3);
+        System.out.println("Área do Quadrado: " + areaQuadrado);
+
+        double areaRetangulo = Quadrilatero.area(5, 5);
+        System.out.println("Área do Retângulo: " + areaRetangulo);
+
+        double areaTrapezio = Quadrilatero.area(7, 8, 9);
+        System.out.println("Área do Trapezio: " + areaTrapezio);
+
+        double areaLosango = Quadrilatero.area(3f, 5f);
+        System.out.println("Área do Losango: " + areaLosango);
+
+    }
+}
+
---
+++ Entendendo Metodos Java/Sobrecargas/Sobrecargas/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        //Quadrilatero
+        System.out.println("Exercício quadrilatero");
+        Quadrilatero.area(5);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(7f, 5f);
+
+    }
+}
