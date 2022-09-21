77b8b58de9acb2b5fc1b00d5c90e1295399b406c
JSilva-Erick <erickjorgesilva@gmail.com>
2022-09-21 11:46:32 -0300

    Cases

--- Estruturas-Condicionais/Estruturas/src/CaixaEletronico.java
+++ Estruturas-Condicionais/Estruturas/src/CaixaEletronico.java
@@ -1,13 +1,17 @@
 public class CaixaEletronico {
     public static void main(String[] args) {
         
-        double saldo = 16.0;
-        double valorSolicitado = 17.0;
+        double saldo = 25.0;
+        double valorSolicitado = 22.0;
 
         if (valorSolicitado < saldo) {
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo " + saldo);
+        } else
+            System.out.println("Saldo insuficiente");
         System.out.println(saldo);            
-        }
+        
+        
     }
     
 }
