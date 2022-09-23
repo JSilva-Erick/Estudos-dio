c4763d2068aaeb31b7f69bbc48f3196f9e69c5ba
JSilva-Erick <erickjorgesilva@gmail.com>
2022-09-23 17:02:01 -0300

    Tratamento de Exceções

---
+++ Estruturas-Condicionais/Estruturas/src/ExemploDoWhile.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class ExemploDoWhile {
+ /*    public static void main(String[] args) {
+        System.out.println("Discando...");
+        do{
+            System.out.println("Telefone tocando");
+        }while(tocando());
+
+        System.out.println("Alô !!!");
+    }
+
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3) == 1;
+        System.out.println("Atendeu? " + atendeu);
+        //negando o ate de continuar tocando
+        return ! atendeu;        
+    } */
+    public static void main(String[] args) {
+        int numero = 1;
+
+        for(int x=1;x<2;x++){
+            numero = numero + x;
+        }
+        System.out.println("O valor de número é: " + numero);
+    }
+    
+}
