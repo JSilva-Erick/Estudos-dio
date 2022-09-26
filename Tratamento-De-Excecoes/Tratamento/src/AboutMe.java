58ac13a54a1fd116e0b4a5ac4ef9dbb9d8b86ff2
JSilva-Erick <erickjorgesilva@gmail.com>
2022-09-26 11:19:20 -0300

    Collections

--- Tratamento-De-Excecoes/Tratamento/src/AboutMe.java
+++ Tratamento-De-Excecoes/Tratamento/src/AboutMe.java
@@ -1,8 +1,10 @@
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class AboutMe {
     public static void main(String[] args) {
+        try{
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Digite seu nome");
@@ -20,5 +22,9 @@ public class AboutMe {
         System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
         System.out.println("Tenho " + idade + " anos ");
         System.out.println("Minha altura é " + altura + " cm");
+        }
+        catch(InputMismatchException e){
+            System.out.println("Os campos idade e altura precisam ser númericos");
+        }
     }
 }
---
+++ Tratamento-De-Excecoes/Tratamento/src/ExemploExecao.java
@@ -0,0 +1,7 @@
+public class ExemploExecao {
+    public static void main(String[] args) {
+        Number valor = Double.valueOf("a.175");
+        System.out.println(valor);
+    }
+    
+}
